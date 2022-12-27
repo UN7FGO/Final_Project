de5999d5bbfabeb704c18947a266c263e98fa087
Gennady <un7fgo@gmail.com>
2022-12-27 21:15:08 +0600

    Добавлен 1-й вариант решения задачи.

---
+++ Task_ver_1/Program.cs
@@ -0,0 +1,28 @@
+﻿string[] array = new string[4] { "Hello", "2", "world", ":-)" };
+// string[] array = new string[4] { "1234", "1567", "-2", "computer science" };
+// string[] array = new string[3] { "Russia", "Denmark", "Kazan" };
+
+Console.WriteLine($"Оригинальный массив: [ {string.Join(", ", array)} ]");
+
+int count = 0;
+
+for (int i = 0; i < array.Length; i++)
+    if (array[i].Length <= 3) count++;
+
+if (count > 0)
+{
+    Console.WriteLine($"В оригинальном массиве, {count} элементов, длинной до трех символов.");
+    string[] result = new string[count];
+    count = 0;
+    for (int i = 0; i < array.Length; i++)
+        if (array[i].Length <= 3)
+        {
+            result[count] = array[i];
+            count++;
+        }
+    Console.WriteLine($"Результирующий массив: [ {string.Join(", ", result)} ]");
+}
+else
+{
+    Console.WriteLine($"В оригинальном массиве нет элементов, длинной до трех символов.");
+}
