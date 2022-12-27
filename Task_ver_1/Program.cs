baef22e2df9e6057c90f795c7a6d308a4edd673e
Gennady <un7fgo@gmail.com>
2022-12-27 21:16:57 +0600

    Добавлен 1-й вариант решения задачи.

--- Task_ver_1/Program.cs
+++ Task_ver_1/Program.cs
@@ -2,6 +2,7 @@
 // string[] array = new string[4] { "1234", "1567", "-2", "computer science" };
 // string[] array = new string[3] { "Russia", "Denmark", "Kazan" };
 
+Console.Clear();
 Console.WriteLine($"Оригинальный массив: [ {string.Join(", ", array)} ]");
 
 int count = 0;
